6cb8d2ee613d893adc45b37619b14639e3ecc2f7
강지수 <123609533+kangjs3779@users.noreply.github.com>
2026-09-18 11:18:03 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        int answer = price;
+        
+        if(100000 <= price && price < 300000) {
+            answer = (int) (price * 0.95);
+        } else if (300000 <= price && price < 500000) {
+            answer = (int) (price * 0.9);
+        } else if (500000 <= price) {
+            answer = (int) (price * 0.8);
+        }
+        
+        
+        return answer;
+    }
+}
